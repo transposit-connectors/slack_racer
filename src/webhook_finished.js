aa9d723db653abe358abdb524cd77121910aeafe
Puya Moayer <puya@transposit.com>
2019-06-07 20:39:32 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -1,7 +1,8 @@
 ({ http_event }) => {
   let qs = require('qs.js');
   let payload = JSON.parse(http_event.parsed_body.payload);
-  
+  console.log(http_event.parsed_body);
+  console.log(payload);
   var result = "fail";
   if (payload.submission.text === payload.submission.input) {
     result = "success"
