6b1f3c8f58a9ee951bc35c8f7140c7069036659a
Puya Moayer <puya@transposit.com>
2019-06-07 19:41:37 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -15,8 +15,5 @@
   };
 
   const trigger_id = slackBody.trigger_id;
-    console.log(slackBody);
-  
-  console.log({ $body: { trigger_id, dialog: JSON.stringify(dialogObj)}});
   return api.run("slack_bot.open_dialog", { $body: { trigger_id, dialog: JSON.stringify(dialogObj)}});
 }
