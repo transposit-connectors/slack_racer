e3e4049f71317e96e63bd38aedb5265f1e72eeaa
Puya Moayer <puya@transposit.com>
2019-06-07 21:48:58 +0000

    Commit from Transposit IDE

--- src/get_random_paragraph.js
+++ src/get_random_paragraph.js
@@ -2,7 +2,7 @@
   const list = api.run('airtable.get_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Meta', recordId: 'recytAa5YjZi1OSJ9'})[0];
   const idx = Math.floor(Math.random()*list.fields.count);
   const entry = api.run('airtable.get_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Texts', recordId: list.fields.texts[idx]})[0];
-  return entry.fields.text;
+  return entry;
 }
 
 /*
