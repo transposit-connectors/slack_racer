a6b7bd197b1cf06e067712407682b1218b5912fe
Puya Moayer <puya@transposit.com>
2019-06-07 21:54:49 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -13,7 +13,10 @@
     var result = 'Fail';
     if (payload.submission.input === payload.submission.original) {
       result = 'Success';
-      
+      const rec = api.run('airtable.get_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Texts', recordId: state.recordId})[0];
+      if (rec.fields.wpm > wpm) {
+        api.run('airtable.update_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Texts', recordId: state.recordId, $body: {fields: {wpm: wpm, user: payload.user.name}}})
+      }
     }
     //console.log(diffs)
     
