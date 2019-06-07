bed199ebc16736a99ac724b9d6058e7ff8cf890c
Puya Moayer <puya@transposit.com>
2019-06-07 18:44:36 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -1,7 +1,6 @@
 (params) => {
-	return api.run('slack_bot.get_bots_info', {bot: "SlackRacer"});
+  return api.run('slack_bot.open_dialog');
 }
-
 /*
  * For sample code and reference material, visit
  * https://www.transposit.com/docs/references/js-operations
