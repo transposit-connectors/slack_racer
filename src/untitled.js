d7aea7995a045a94e7351a357d94c5dc56dd3b36
Puya Moayer <puya@transposit.com>
2019-06-07 20:44:08 +0000

    Commit from Transposit IDE

---
+++ src/untitled.js
@@ -0,0 +1,44 @@
+(params) => {
+  var x = params.input;
+  var y = params.original;
+
+  x=x.split(' ');
+  y=y.split(' ');
+
+  var i=0,j=0;
+  while (1) {
+    if (!x[i] || !y[j]) break;
+
+    if (x[i] == y[j]) {
+      i++;
+      j++;
+      continue;
+    }
+
+    if (x[i] == y[j+1]) {
+      console.log('Extra word : ', y[j]);
+      i++;
+      j+=2;
+      continue;
+    }
+
+    if (x[i+1] == y[j]) {
+      console.log('Skip word: ', x[i]);
+      i+=2;
+      j++;
+      continue;
+    }
+
+    if (x[i+1] == y[j+1]) {
+      console.log('Wrong word: ', y[j]);
+      i++;
+      j++;
+      continue;
+    }
+  }
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
