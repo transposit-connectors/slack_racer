8608fa9c9c68c6e7c832ba79f59bd7dbffcced10
Puya Moayer <puya@transposit.com>
2019-06-07 18:44:54 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -1,5 +1,5 @@
 (params) => {
-  return api.run('slack_bot.open_dialog');
+  return api.run('slack_bot.open_dialog', {dialog: "asd", trigger_id: "ASD"});
 }
 /*
  * For sample code and reference material, visit
