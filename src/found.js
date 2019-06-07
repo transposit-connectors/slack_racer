3f612a88aa7bcf53b0f865d68003a03df96eb6be
Puya Moayer <puya@transposit.com>
2019-06-07 17:55:01 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -1,12 +1,7 @@
 ({slackBody}) => {
-  const im = api.run('slack_bot.open_im', {$body: {
-    "return_im": true,
-    "include_locale": true,
-    "user": slackBody.user_id,
-  }})[0];
   let text = api.run('this.get_random_paragraph')[0];
   let post = {
-    channel: 'DGNG13MQB',//slackBody.channel_id,
+    channel: slackBody.channel_id,
   	user: slackBody.user_id,
     text: `You've run the slack command`,
     blocks: [{
