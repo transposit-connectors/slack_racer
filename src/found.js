3bfa14f8663dfe647751b4340c92ddae4ec50afd
Puya Moayer <puya@transposit.com>
2019-06-07 16:43:23 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -1,7 +1,3 @@
-/*
- * This method is called running as the transposit user who has configured their slack user to match
- * the incoming webhook message. For convenience, we pass in the body from the slack webhook.
- */
 ({slackBody}) => {
   let post = {
     channel: slackBody.channel_id,
