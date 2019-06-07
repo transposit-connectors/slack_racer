67946c0903d39fde3e2e020ea80d81057926344b
Puya Moayer <puya@transposit.com>
2019-06-07 19:34:22 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -7,18 +7,11 @@
     label: "Write this",
   }];
   
-  const state = JSON.stringify({
-    sender: user,
-    ts,
-    content: text.substring(0,2900)
-  })
-  
   const dialogObj = {
     callback_id: "send_report",
     notify_on_cancel: false,
     title: "Report Message",
     elements,
-    state
   };
 
   const trigger_id = slackBody.trigger_id;
