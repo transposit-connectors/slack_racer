a3d4b47f67efbe54db13b371bfa3d933c450df31
Puya Moayer <puya@transposit.com>
2019-06-07 20:41:59 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++
@@ -1,6 +0,0 @@
-(params) => {
-}
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/references/js-operations
- */
--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -1,12 +1,28 @@
 ({ http_event }) => {
-  let qs = require('qs.js');
-  let payload = JSON.parse(http_event.parsed_body.payload);
-  console.log(http_event.parsed_body);
-  console.log(payload);
-  var result = "fail";
-  if (payload.submission.text === payload.submission.input) {
-    result = "success"
-  }
+  setImmediate(() => {
+    let qs = require('qs.js');
+    let payload = JSON.parse(http_event.parsed_body.payload);
+    console.log(http_event.parsed_body);
+    console.log(payload);
+    var result = "fail";
+    if (payload.submission.text === payload.submission.input) {
+      result = "success"
+    }
+    
+    let post = {
+      channel: payload.channel.id,
+      user: payload.user.id,
+      text: `You've run the slack command`,
+      blocks: [{
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": events === '' ? 'You have no events today' : events
+        },
+      }]
+    }
+    return api.run('slack_bot.post_chat_ephemeral', {$body: post});
+  });
   return { status_code: 200 };
 }
 
