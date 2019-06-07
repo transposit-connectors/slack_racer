3f41f2126d8792441841b92c9a139007df374614
Puya Moayer <puya@transposit.com>
2019-06-07 18:47:01 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -1,23 +1,23 @@
 ({slackBody}) => {
   console.log(slackBody);
     return api.run('slack_bot.open_dialog', {dialog: {body: {
-  "callback_id": "ryde-46e2b0",
-  "title": "Request a Ride",
-  "submit_label": "Request",
-  "state": "Limo",
-  "elements": [
-    {
-      "type": "text",
-      "label": "Pickup Location",
-      "name": "loc_origin"
-    },
-    {
-      "type": "text",
-      "label": "Dropoff Location",
-      "name": "loc_destination"
-    }
-  ]
-}}, trigger_id: slackBody.trigger_id});
+      "callback_id": "ryde-46e2b0",
+      "title": "Request a Ride",
+      "submit_label": "Request",
+      "state": "Limo",
+      "elements": [
+        {
+          "type": "text",
+          "label": "Pickup Location",
+          "name": "loc_origin"
+        },
+        {
+          "type": "text",
+          "label": "Dropoff Location",
+          "name": "loc_destination"
+        }
+      ]
+    }}, trigger_id: slackBody.trigger_id});
   let text = api.run('this.get_random_paragraph')[0];
   let post = {
     channel: slackBody.channel_id,
