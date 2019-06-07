a98939479e033b853fe9830a4aae9283e6a77c54
Puya Moayer <puya@transposit.com>
2019-06-07 17:20:48 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -1,11 +1,10 @@
 ({ http_event }) => {
   let qs = require('qs.js');
   let body = JSON.parse(qs.parse(http_event).body);
-  console.log(body);
-  api.run('airtable.create_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Texts', $body: {fields: {id: 12, text: body.challenge}}});
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
+    challenge: body.challenge,
     body: { "greeting": "Hello World" }
   };
 }
