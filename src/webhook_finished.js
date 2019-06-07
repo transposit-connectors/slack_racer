6feab21eff32ba878ee3814d1080f29d34bfd4b0
Puya Moayer <puya@transposit.com>
2019-06-07 17:13:21 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -1,4 +1,5 @@
 ({ http_event }) => {
+  api.run('airtable.create_recoerd', {baseId: 'appcX3FvaawpLi3eF', table: 'Texts', $body: {fields: {id: "12", text: http_event}}});
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
