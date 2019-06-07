625e09bb2dd8bf780eff272f7c997004a84e4910
Puya Moayer <puya@transposit.com>
2019-06-07 18:35:24 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -4,14 +4,7 @@
   let post = {
     channel: slackBody.channel_id,
   	user: slackBody.user_id,
-    text: `You've run the slack command`,
-    blocks: [{
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": text,
-      },
-    }]
+    text: text,
   }
   
   return api.run('slack_bot.post_chat_ephemeral', {$body: post});
