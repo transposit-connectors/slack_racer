1bee60ce44daac6c75ede8f629a753afa7510483
Puya Moayer <puya@transposit.com>
2019-09-13 23:38:04 +0000

    Commit from Transposit IDE

---
+++ src/slack_post_chat_ephemeral.js
@@ -0,0 +1,10 @@
+(params) => {
+  return {
+    mission: "complete"
+  };
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
