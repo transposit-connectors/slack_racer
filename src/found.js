e66fa40aaa9ef835a711df88cfbc1c84976916e1
Puya Moayer <puya@transposit.com>
2019-09-13 21:27:05 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -1,5 +1,8 @@
 ({slackBody}) => {
   
+  /*
+   * Fetch paragraph text from Airtable
+   */
   const list = api.run('airtable.get_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Meta', recordId: 'recytAa5YjZi1OSJ9'})[0];
   const idx = Math.floor(Math.random()*list.fields.count);
   let rec = api.run('airtable.get_record', {baseId: 'appcX3FvaawpLi3eF', table: 'Texts', recordId: list.fields.texts[idx]})[0];
