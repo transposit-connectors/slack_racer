1dd378264ff1873c0b592d039ba3fc523278aafb
Puya Moayer <puya@transposit.com>
2019-09-13 23:32:53 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -15,7 +15,7 @@
     const newer = payload.action_ts
 	const wpm = Math.floor(payload.submission.original.split(' ').length / ((newer - old) / 60))
     
-    // validate input
+    // validate input and determing result
     const userInput = payload.submission.input.trim();
     var result = 'Sorry, input does not match!';
     if (payload.submission.input === payload.submission.original) {
