bc88d5dc10a131f57723c37f0f7580a84f31a4a7
Puya Moayer <puya@transposit.com>
2019-09-13 22:58:41 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -2,7 +2,8 @@
   console.log(http_event);
   let body = http_event.parsed_body;
   // setImmediate(() => {
-    const dialogJson = api.run("this.generate_dialog_json", {slackBody: body});  
+    const dialogJson = api.run("this.generate_dialog_json", {slackBody: body}); 
+  	console.log(dialogJson);
     // console.log(dialogJson);
     // console.log(api.run("slack.open_dialog", { $body: { trigger_id: body.trigger_id, dialog: dialogJson }}, { "asGroup": body.team_id }));
   // });
