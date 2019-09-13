fd93ccac8f1913d6d4f939ca7b6052009e02e0e6
Puya Moayer <puya@transposit.com>
2019-09-13 23:16:10 +0000

    Commit from Transposit IDE

--- src/found.js
+++ src/found.js
@@ -36,9 +36,5 @@
     state
   };
 
-  // strangely, cannot pass json between operations?
-  const dialogJson = JSON.stringify(dialog);
-  // api.run("slack.open_dialog", { $body: { trigger_id: slackBody.trigger_id, dialog: dialogJson }}, { "asGroup": slackBody.team_id });
-  console.log(dialogJson);
-  return dialogJson;
+  return JSON.stringify(dialog);
 }
