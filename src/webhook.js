14a822f7bd49acf16397cf50cf51a04d5ddbbef2
Puya Moayer <puya@transposit.com>
2019-09-13 21:19:57 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -3,7 +3,7 @@
   let body = http_event.parsed_body;
   setImmediate(() => {
     const { channel_id, team_id, user_id } = body;
-    api.run("this.serve_text", {slackBody: body}, {userid: user.id});
+    api.run("this.serve_text", {slackBody: body});
   });
   return { status_code: 200 };
 }
