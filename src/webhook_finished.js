bb0dab89b4dcdc8a217368d7d6aa87163e6a4c68
Puya Moayer <puya@transposit.com>
2019-09-14 00:22:05 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -62,7 +62,7 @@
   // return to Slack right away to prevent timeout
   return {
     status_code: 200,
-    // headers: { "Content-Type": "application/json" },
-    // body: { message: {}}
+    headers: { "Content-Type": "application/json" },
+    body: { message: {}}
   };
 }
