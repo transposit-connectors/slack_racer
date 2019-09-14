bacbb228f27b4b6b9af7064a53ac18250a97b450
Puya Moayer <puya@transposit.com>
2019-09-14 00:19:57 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -23,7 +23,7 @@
         user: payload.user.id,
         text: `${wpm} wpm? Something smells fishy :fish: :face_with_monocle:` 
       }
-      return api.run('slack.post_chat_ephemeral', {$body: msg});
+      return api.run('slack.post_chat_ephemeral', {$body: msg}, {asGroup: payload.team.id});
     }
 
     console.log("not hacking");
@@ -54,7 +54,7 @@
       user: payload.user.id,
       text: `${result} Wpm = ${wpm}` 
     }
-    return api.run('slack.post_chat_ephemeral', {$body: post});
+    return api.run('slack.post_chat_ephemeral', {$body: post}, {asGroup: payload.team.id});
   });
   
   // return to Slack right away to prevent timeout
