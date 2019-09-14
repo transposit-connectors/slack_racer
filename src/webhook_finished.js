7125092470ebdf22712fc5ece616e93196a0806f
Puya Moayer <puya@transposit.com>
2019-09-14 00:01:19 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -1,4 +1,10 @@
 ({ http_event }) => {
+  
+  return {
+    status_code: 200,
+    headers: { "Content-Type": "application/json" },
+    body: { "greeting": "Hello World" }
+  };
   setImmediate(() => {
     /*
      * Parse submission 
