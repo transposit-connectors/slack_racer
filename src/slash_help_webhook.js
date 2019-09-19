959265113e274abd32d462217ee12193d49677a7
Puya Moayer <puya@transposit.com>
2019-09-19 18:25:16 +0000

    Commit from Transposit IDE

--- src/slash_help_webhook.js
+++ src/slash_help_webhook.js
@@ -2,7 +2,7 @@
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
-    body: { "greeting": "Hello World" }
+    body: { "text": "Please e-mail puya@transposit.com with any help inquiries or feedback!" }
   };
 }
 
