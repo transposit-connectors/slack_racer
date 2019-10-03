b22e9e4458dba7c830cb26486f6172d71fd8ad35
Puya Moayer <puya@transposit.com>
2019-10-03 21:32:44 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++ src/untitled_1.js
@@ -2,10 +2,11 @@
   let rec = api.run('airtable.get_records', {baseId: env.get("baseId"), table: 'Workspaces', filterByFormula: `id="${params.workspaceId}"`})[0];
   let blob = rec.fields[params.paragraphId]
   
-  let meta = {};
+  let meta = {username: "", wpm: -1};
   if (blob ~= null) {
   	meta = JSON.parse(rec.fields[params.paragraphId]);  
   }
+  
   
   return meta
 }
