f100c7d98519b5cc4acdcf44f094eaa8a7cf8e2d
Puya Moayer <puya@transposit.com>
2019-10-03 23:57:34 +0000

    Commit from Transposit IDE

--- src/generate_diff.js
+++ src/generate_diff.js
@@ -1,52 +1,51 @@
-({input, original, wpm}) => {
-	return [
+({ input, original, wpm }) => {
+  return [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `Sorry, your input was incorrect! *Wpm:* ${wpm}`
+      }
+    },
+    {
+      "type": "context",
+      "elements": [
         {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `Sorry, your input was incorrect! *Wpm:* ${wpm}`
-			}
-		},
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "mrkdwn",
-					"text": "Input"
-				}
-			]
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `>>>${input}`
-			}
-            
-		},
-		{
-			"type": "divider"
-		},
-		{
-			"type": "context",
-			"elements": [
-				{
-					"type": "mrkdwn",
-					"text": "Prompt (Text #15)"
-				}
-			]
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": `>>>${original}`
-			}
-		},
+          "type": "mrkdwn",
+          "text": "Input"
+        }
+      ]
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `>>>${input}`
+      }
+    },
+    {
+      "type": "divider"
+    },
+    {
+      "type": "context",
+      "elements": [
         {
-			"type": "divider"
-		}
-	];
+          "type": "mrkdwn",
+          "text": "Prompt (Text #15)"
+        }
+      ]
+    },
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `>>>${original}`
+      }
+    },
+    {
+      "type": "divider"
+    }
+  ];
 }
 
 /*
