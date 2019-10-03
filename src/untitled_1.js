30b0462a05c040e04407a777ad6869917cc76bd9
Puya Moayer <puya@transposit.com>
2019-10-03 23:26:00 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++ src/untitled_1.js
@@ -17,7 +17,7 @@
     fields.id = workspaceId;
     fields[textId] = JSON.stringify({ username, wpm });
     api.run("airtable.create_record", { baseId: env.get("baseId"), table: "Workspaces", $body: { fields } });
-	return { updated: true, message: `You now hold the record for text #${textId}! :crown: :tada: `};
+	return { updated: true, message: `You now hold the record for text #${textId}! :crown: :tada:`};
   }
   
   // parse json
