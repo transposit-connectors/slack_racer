c7d088dd4f129fc7236fe2ca71af206a84f15846
Puya Moayer <puya@transposit.com>
2019-10-03 00:22:15 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -16,7 +16,7 @@
 	const wpm = Math.floor(payload.submission.original.split(' ').length / ((newer - old) / 60))
     
     // advanced anti-hack detection
-    if (wpm > 220) {
+    if (wpm > 1000000000000) {
       let msg = {
         channel: payload.channel.id,
         user: payload.user.id,
