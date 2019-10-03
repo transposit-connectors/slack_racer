26f81f1e5c2f11a6d4f8bee6c97d3b53d0cfd2be
Puya Moayer <puya@transposit.com>
2019-10-03 00:27:23 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -68,54 +68,6 @@
     return str;
   }
 
-  function randomColor() {
-      return "rgb(" + (Math.random() * 100) + "%, " + 
-                      (Math.random() * 100) + "%, " + 
-                      (Math.random() * 100) + "%)";
-  }
-  function diffString2( o, n ) {
-    o = o.replace(/\s+$/, '');
-    n = n.replace(/\s+$/, '');
-
-    var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/) );
-
-    var oSpace = o.match(/\s+/g);
-    if (oSpace == null) {
-      oSpace = ["\n"];
-    } else {
-      oSpace.push("\n");
-    }
-    var nSpace = n.match(/\s+/g);
-    if (nSpace == null) {
-      nSpace = ["\n"];
-    } else {
-      nSpace.push("\n");
-    }
-
-    var os = "";
-    var colors = new Array();
-    for (var i = 0; i < out.o.length; i++) {
-        colors[i] = randomColor();
-
-        if (out.o[i].text != null) {
-            os += escape(out.o[i].text) + oSpace[i] + "</span>";
-        } else {
-            os += "~" + escape(out.o[i]) + "~" + oSpace[i];
-        }
-    }
-
-    var ns = "";
-    for (var i = 0; i < out.n.length; i++) {
-        if (out.n[i].text != null) {
-            ns += escape(out.n[i].text) + nSpace[i] + "</span>";
-        } else {
-            ns += "*" + escape(out.n[i]) + "*";
-        }
-    }
-
-    return { o : os , n : ns };
-  }
-
   function diff( o, n ) {
     var ns = new Object();
     var os = new Object();
