52b72624c4e89f13e351ecfa3d4f4b1d2b4a2870
Puya Moayer <puya@transposit.com>
2019-10-03 23:53:32 +0000

    Commit from Transposit IDE

--- src/generate_diff.js
+++ src/generate_diff.js
@@ -1,4 +1,4 @@
-(params) => {
+({input, original}) => {
 	return [
 		{
 			"type": "context",
@@ -13,7 +13,7 @@
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": ">>>This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
+				"text": `>>>${input}`
 			}
             
 		},
@@ -33,7 +33,7 @@
 			"type": "section",
 			"text": {
 				"type": "mrkdwn",
-				"text": ">>>This is a plain text section block."
+				"text": `>>>${original}`
 			}
 		}
 	];
