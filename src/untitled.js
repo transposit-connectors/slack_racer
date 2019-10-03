6217670dd58e2467c49e906f32a84e7e6203b2c5
Puya Moayer <puya@transposit.com>
2019-10-03 19:32:30 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -1389,10 +1389,10 @@ params => {
         .replace(pattern_para, "&para;<br>");
       switch (op) {
         case DIFF_INSERT:
-          html[x] = '<b>*' + text.trim()  + "*</b> ";
+          html[x] = '\u2063*' + text.trim()  + "* ";
           break;
         case DIFF_DELETE:
-          html[x] = '\u000b~' + text.trim() + "~ ";
+          html[x] = '\u2063~' + text.trim() + "~ ";
           break;
         case DIFF_EQUAL:
           html[x] = text;
