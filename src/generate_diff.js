24700d1658f899f9cd831396f6ca6590a925a510
Puya Moayer <puya@transposit.com>
2019-10-03 23:52:51 +0000

    Commit from Transposit IDE

--- src/generate_diff.js
+++ src/generate_diff.js
@@ -1,7 +1,42 @@
 (params) => {
-  return {
-    mission: "complete"
-  };
+	return [
+		{
+			"type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": "Input"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": ">>>This is a mrkdwn section block :ghost: *this is bold*, and ~this is crossed out~, and <https://google.com|this is a link>"
+			}
+            
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "context",
+			"elements": [
+				{
+					"type": "mrkdwn",
+					"text": "Prompt (Text #15)"
+				}
+			]
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": ">>>This is a plain text section block."
+			}
+		}
+	];
 }
 
 /*
