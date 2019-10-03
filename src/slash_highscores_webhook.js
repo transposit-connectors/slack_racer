d14e7b64a20efcd0a79a1aaecf734a50d4caa5e3
Puya Moayer <puya@transposit.com>
2019-10-03 22:36:48 +0000

    Commit from Transposit IDE

--- src/slash_highscores_webhook.js
+++ src/slash_highscores_webhook.js
@@ -4,10 +4,27 @@
   let body = http_event.parsed_body;
   // console.log(body)
   
-  let rec = api.run('airtable.get_records', {baseId: env.get("baseId"), table: 'Workspaces', filterByFormula: `id="${body.team_id}"`});
+  let rec = api.run('airtable.get_records', {baseId: env.get("baseId"), table: 'Workspaces', filterByFormula: `id="${body.team_id}"`})[0];
   let blocks = [];
-  console.log(rec);
-  Object.values(rec.fields)//.forEach(() => console.log("asd"))
+  console.log(rec)
+  console.log(rec.fields);
+  for (field in rec.fields) {
+    if (parseInt(field)) {
+      blocks.push(
+        {
+			"type": "section",
+			"text": {
+				"type": "plain_text",
+				"text": "This is a plain text section block.",
+				"emoji": true
+			}
+		})
+      blocks.push(
+		{
+			"type": "divider"
+		})
+    }
+  }
   
   
   
