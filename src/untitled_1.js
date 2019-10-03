4f52ded9bf0b141bea91cefa818603c41068424a
Puya Moayer <puya@transposit.com>
2019-10-03 23:00:47 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++ src/untitled_1.js
@@ -1,34 +1,42 @@
-({workspaceId, paragraphId, username, wpm}) => {
-  let rec = api.run('airtable.get_records', {baseId: env.get("baseId"), table: 'Workspaces', filterByFormula: `id="${workspaceId}"`})[0];
-  
-  
+({ workspaceId, paragraphId, username, wpm }) => {
+  let rec = api.run("airtable.get_records", {
+    baseId: env.get("baseId"),
+    table: "Workspaces",
+    filterByFormula: `id="${workspaceId}"`
+  })[0];
+
   // create record for this workspace if first time
   if (rec == null) {
     let fields = {};
     fields.id = workspaceId;
-    fields[paragraphId] = JSON.stringify({username, wpm})
-    api.run('airtable.create_record', {baseId: env.get("baseId"), table: 'Workspaces', $body: {fields}})
+    fields[paragraphId] = JSON.stringify({ username, wpm });
+    api.run("airtable.create_record", { baseId: env.get("baseId"), table: "Workspaces", $body: { fields } });
     return;
   }
-  let blob = rec.fields[paragraphId]
-  
-  let meta = {username: "", wpm: -1};
+  let blob = rec.fields[paragraphId];
+
+  let meta = { username: "", wpm: -1 };
   if (blob != null) {
-  	meta = JSON.parse(rec.fields[paragraphId]);  
+    meta = JSON.parse(rec.fields[paragraphId]);
   }
-  
+
   let oldName = meta.username;
   if (meta.wpm < wpm) {
     meta.username = username;
     meta.wpm = wpm;
-    
+
     let fields = {};
     fields[paragraphId] = JSON.stringify(meta);
-    api.run('airtable.update_record', {baseId: env.get("baseId"), table: 'Workspaces', recordId: rec.id, $body: {fields}});
-    return {updated: true, oldName};
+    api.run("airtable.update_record", {
+      baseId: env.get("baseId"),
+      table: "Workspaces",
+      recordId: rec.id,
+      $body: { fields }
+    });
+    return { updated: true, oldName };
   }
-  return {updated: false};
-}
+  return { updated: false };
+};
 
 /*
  * For sample code and reference material, visit
