cd8812a72b5c94d93d29bcad6575d71c15d90d57
Puya Moayer <puya@transposit.com>
2019-10-03 23:00:03 +0000

    Commit from Transposit IDE

--- src/slash_highscores_webhook.js
+++ src/slash_highscores_webhook.js
@@ -2,31 +2,33 @@
   setImmediate(() => {
     var _ = require("underscore.js");
     let body = http_event.parsed_body;
-    console.log(body)
+    console.log(body);
 
     let rec = api.run("airtable.get_records", {
       baseId: env.get("baseId"),
       table: "Workspaces",
       filterByFormula: `id="${body.team_id}"`
     })[0];
-    
+
     if (rec == null) {
       return api.run(
-      "slack.post_chat_ephemeral",
-      { $body: {
-              channel: body.channel_id,
-      user: body.user_id,
-				"text": "No high scores! Please finish a race first."
-      } },
-      { asGroup: body.team_id }
-    );
+        "slack.post_chat_ephemeral",
+        {
+          $body: {
+            channel: body.channel_id,
+            user: body.user_id,
+            text: "No high scores! Please finish a race first."
+          }
+        },
+        { asGroup: body.team_id }
+      );
     }
-    
+
     let blocks = [];
 
     for (key in rec.fields) {
       if (parseInt(key)) {
-        let blob = JSON.parse(rec.fields[key])
+        let blob = JSON.parse(rec.fields[key]);
         blocks.push({
           type: "section",
           text: {
@@ -41,30 +43,23 @@
     }
 
     if (blocks.length === 0) {
-      blocks.push(
-        {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "No high scores! Please finish a race first."
-			}
-		}
-      )
+      blocks.push({
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "No high scores! Please finish a race first."
+        }
+      });
     }
-    
+
     let post = {
       channel: body.channel_id,
       user: body.user_id,
       blocks: blocks
     };
 
-    return api.run(
-      "slack.post_chat_ephemeral",
-      { $body: post },
-      { asGroup: body.team_id }
-    );
+    return api.run("slack.post_chat_ephemeral", { $body: post }, { asGroup: body.team_id });
   });
 
   return { status_code: 200 };
-}
-
+};
