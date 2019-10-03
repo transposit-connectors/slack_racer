8ef643f5cef8dc60cbea850b9af1fee10d743e0b
Puya Moayer <puya@transposit.com>
2019-10-03 00:14:11 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -51,7 +51,7 @@
       text: `${result} \n*Wpm = ${wpm}*` 
     }
     
-    console.log(post);
+    api.log(post);
     return api.run('slack.post_chat_ephemeral', {$body: post}, {asGroup: payload.team.id});
   });
   
