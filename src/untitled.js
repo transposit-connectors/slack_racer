b5a08eea2fb5c40add16c3c3813036c126741968
Puya Moayer <puya@transposit.com>
2019-10-03 23:19:47 +0000

    Commit from Transposit IDE

--- src/untitled.js
+++ src/untitled.js
@@ -2423,6 +2423,5 @@ params => {
   var dmp = new diff_match_patch();
   let diff = dmp.diff_main(params.input, params.original);
   dmp.diff_cleanupSemantic(diff);
-  console.log(diff)
   return dmp.diff_prettyHtml(diff);
 }
