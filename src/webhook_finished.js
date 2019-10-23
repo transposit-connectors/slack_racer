541204ef96de586486dea6478592b974f8e78b95
Puya Moayer <puya@transposit.com>
2019-10-23 20:55:56 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -15,7 +15,7 @@
     
     // pull out submission (dependent on order of blocks; for speed)
     let input = view.state.values.input.input.value;
-    let original = 
+    // let original = 
 
     // calculate timings
     const old = metadata.ts / 1000;
