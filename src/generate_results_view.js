eff205bf1c965d319ca9b221b3fc4a574e8902db
Puya Moayer <puya@transposit.com>
2019-10-23 22:36:05 +0000

    Commit from Transposit IDE

--- src/generate_results_view.js
+++ src/generate_results_view.js
@@ -1,23 +1,38 @@
 ({ testView, result }) => {
-  	let blocks = testView.blocks;	
-	let resultBlocks = [
-      {
-          "type": "divider"
-      },
-      {
-          "type": "section",
-          "block_id": "original",
-          "text": {
-              "type": "mrkdwn",
-              "text": `${result}`
-          }
-      }
-    ]
-    blocks.concat(resultBlocks)
-  	
-  	let view = {
-      
+  let blocks = testView.blocks;	
+  let resultBlocks = [
+    {
+        "type": "divider"
+    },
+    {
+        "type": "section",
+        "block_id": "original",
+        "text": {
+            "type": "mrkdwn",
+            "text": `${result}`
+        }
     }
+  ]
+  blocks.concat(resultBlocks)
+  	
+  let view = {
+	type: "modal",
+    callback_id: "results",
+    title: 
+    {
+      type: "plain_text",
+      text: `SlackRacer: Text ${textId}`
+    },
+    "close": {
+    "type": "plain_text",
+    "text": "Cancel"
+    },
+    "submit": {
+      "type": "plain_text",
+      "text": "Retry"
+    },
+    blocks
+  };
 }
 
 /*
