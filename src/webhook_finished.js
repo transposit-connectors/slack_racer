f34a07f399b7cb0f578810fcb1df9f1421be745e
Puya Moayer <puya@transposit.com>
2019-10-24 20:58:42 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -55,7 +55,7 @@
     // notify user of result
     let resultsView = api.run("this.generate_results_view", {testView: view, input, result})[0];
 
-  	console.log(api.run("slack.views_push", {trigger_id: payload.trigger_id, view: resultsView}))
+  	console.log(api.run("slack.views_push", {trigger_id: payload.trigger_id, view: resultsView}, {asGroup: payload.team.id}))
     // using response_action, must respond with everything within 3 seconds
     // return {
     //   status_code: 200,
