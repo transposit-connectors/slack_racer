fc75d9f18710e4391ef4b0fc19710911753b11bf
Puya Moayer <puya@transposit.com>
2019-10-24 22:42:24 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -3,51 +3,55 @@
  */
 
 ({ http_event }) => {
-    // parse submission
-    let qs = require("qs.js");
-    let payload = JSON.parse(http_event.parsed_body.payload);
-    console.log(payload);
-    let view = payload.view;
-    let type = view.callback_id;  
-  	let metadata = JSON.parse(view.private_metadata);
-                       
-    // pull out submission (dependent on order of blocks; for speed)                   
-    let input = view.state.values.input.input.value;
-    let original = view.blocks[0].text.text;
+  // parse submission
+  let qs = require("qs.js");
+  let payload = JSON.parse(http_event.parsed_body.payload);
+  console.log(payload);
+  let view = payload.view;
+  let type = view.callback_id;
+  let metadata = JSON.parse(view.private_metadata);
 
-    // calculate timings
-    const old = metadata.ts / 1000;
-    const newer = Date.now() / 1000;
-    const wpm = Math.floor(original.split(" ").length / ((newer - old) / 60));
+  // pull out submission (dependent on order of blocks; for speed)
+  let input = view.state.values.input.input.value;
+  let original = view.blocks[0].text.text;
 
-    // validate and determine result
-    const userInput = input.trim();
-    let result;  
-  
-  console.log(input)
-  console.log(original)
-    // advanced anti-hack detection
-    if (wpm > 30000000) {
-      result = `*${wpm} wpm?* Something smells fishy :fish: :face_with_monocle:`;
-    } else if (input === original) {
-      // recordkeeping
-      result = api.run("this.update_best_record", {
-        workspaceId: payload.team.id,
-        textId: metadata.textId,
-        username: payload.user.name,
-        wpm
-      })[0].message;
-    } else {
-      result = `Sorry, input did not match! *${wpm} wpm*`;
-      /* generate diff here */
+  // calculate timings
+  const old = metadata.ts / 1000;
+  const newer = Date.now() / 1000;
+  const wpm = Math.floor(original.split(" ").length / ((newer - old) / 60));
+
+  // validate and determine result
+  const userInput = input.trim();
+  let result;
+
+  console.log(input);
+  console.log(original);
+  // advanced anti-hack detection
+  if (wpm > 30000000) {
+    result = `*${wpm} wpm?* Something smells fishy :fish: :face_with_monocle:`;
+  } else if (input === original) {
+    // recordkeeping
+    result = api.run("this.update_best_record", {
+      workspaceId: payload.team.id,
+      textId: metadata.textId,
+      username: payload.user.name,
+      wpm
+    })[0].message;
+  } else {
+    result = `Sorry, input did not match! *${wpm} wpm*`;
+    /* generate diff here */
+  }
+
+  // using response_action, must respond with everything within 3 seconds
+  return {
+    status_code: 200,
+    body: {
+      response_action: "update",
+      view: api.run("this.generate_results_view", {
+        testView: view,
+        input,
+        result
+      })[0]
     }
-
-    // using response_action, must respond with everything within 3 seconds
-    return {
-      status_code: 200,
-      body: {
-        response_action: "update",
-        view: api.run("this.generate_results_view", {testView: view, input, result})[0]
-      }
-    }
+  };
 }
