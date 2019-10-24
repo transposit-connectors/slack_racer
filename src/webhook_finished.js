c236b6e42d9790ec92c9b5ae19222972b24ff41b
Puya Moayer <puya@transposit.com>
2019-10-24 20:47:56 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -18,7 +18,7 @@
         status_code: 200,
         body: {
           response_action: "update",
-          view: api.run('this.generate_test_view')[0];
+          view: api.run('this.generate_test_view')[0]
         }
       }    
     }
