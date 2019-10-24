ff027be90f05fc1ece0a891f305157a490296060
Puya Moayer <puya@transposit.com>
2019-10-24 21:20:59 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -15,7 +15,7 @@
       setImmediate(() => {
         let testView = api.run("this.generate_test_view", {stringify: true})[0];
         console.log(testView);
-        console.log(api.run("slack.views_update", {$body: {view_id: payload.root_view_id, view: testView}}, {asGroup: payload.team.id}));
+        console.log(api.run("slack.views_update", {$body: {view_id: view.root_view_id, view: testView}}, {asGroup: payload.team.id}));
       });
       return {status_code: 200}
     }
