190d347f9e5df58c1c709836be17b4915704243e
Puya Moayer <puya@transposit.com>
2019-10-30 17:16:08 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -3,45 +3,46 @@
  */
 
 ({ http_event }) => {
-  // parse submission
+    // parse submission
   let qs = require("qs.js");
   let payload = JSON.parse(http_event.parsed_body.payload);
   console.log(payload);
   let view = payload.view;
   let type = view.callback_id;
   let metadata = JSON.parse(view.private_metadata);
+  setImmediate(() => {
+    // pull out submission (dependent on order of blocks; for speed)
+    let input = view.state.values.input.input.value;
+    let original = view.blocks[0].text.text;
 
-  // pull out submission (dependent on order of blocks; for speed)
-  let input = view.state.values.input.input.value;
-  let original = view.blocks[0].text.text;
+    // calculate timings
+    const old = metadata.ts / 1000;
+    const newer = Date.now() / 1000;
+    const wpm = Math.floor(original.split(" ").length / ((newer - old) / 60));
 
-  // calculate timings
-  const old = metadata.ts / 1000;
-  const newer = Date.now() / 1000;
-  const wpm = Math.floor(original.split(" ").length / ((newer - old) / 60));
+    // validate and determine result
+    const userInput = input.trim();
+    let result;
 
-  // validate and determine result
-  const userInput = input.trim();
-  let result;
-
-  // advanced anti-hack detection
-  if (wpm > 250) {
-    result = `*${wpm} wpm?* Something smells fishy :fish: :face_with_monocle:`;
-  } else if (input === original) {
-    // recordkeeping
-    result = api.run("this.update_best_record", {
-      workspaceId: payload.team.id,
-      textId: metadata.textId,
-      username: payload.user.name,
-      wpm
-    })[0].message;
-  } else {
-    result = `Sorry, input did not match! *${wpm} wpm*`;
-    /* generate diff here */
-  }
+    // advanced anti-hack detection
+    if (wpm > 250) {
+      result = `*${wpm} wpm?* Something smells fishy :fish: :face_with_monocle:`;
+    } else if (input === original) {
+      // recordkeeping
+      result = api.run("this.update_best_record", {
+        workspaceId: payload.team.id,
+        textId: metadata.textId,
+        username: payload.user.name,
+        wpm
+      })[0].message;
+    } else {
+      result = `Sorry, input did not match! *${wpm} wpm*`;
+      /* generate diff here */
+    }
+  })
 
   // using response_action, must respond with everything within 3 seconds
-  return api.run("this.ack_loading_screen")[0];
+  return api.run("this.ack_loading_screen", {title: view.title})[0];
   // return {
   //   status_code: 200,
   //   body: {
