8ad0fa67ff801d48b933c17ff3dce4a518288f4b
Puya Moayer <puya@transposit.com>
2019-10-30 18:11:44 +0000

    Commit from Transposit IDE

--- src/generate_results_view.js
+++ src/generate_results_view.js
@@ -1,96 +1,75 @@
 ({ payload }) => {
-    let view = payload.view;
-  	let type = view.callback_id;
-  	let metadata = JSON.parse(view.private_metadata);
-  
-  	// pull out submission (dependent on order of blocks; for speed)
-    let input = view.state.values.input.input.value;
-    let original = view.blocks[0].text.text;
+  let view = payload.view;
+  let type = view.callback_id;
+  let metadata = JSON.parse(view.private_metadata);
 
-    // calculate timings
-    const old = metadata.ts / 1000;
-    const newer = Date.now() / 1000;
-    const wpm = Math.floor(original.split(" ").length / ((newer - old) / 60));
+  // pull out submission (dependent on order of blocks; for speed)
+  let input = view.state.values.input.input.value;
+  let original = view.blocks[0].text.text;
 
-    // validate and determine result
-    const userInput = input.trim();
-    let result;
+  // calculate timings
+  const old = metadata.ts / 1000;
+  const newer = Date.now() / 1000;
+  const wpm = Math.floor(original.split(" ").length / ((newer - old) / 60));
 
-    // advanced anti-hack detection
-    if (wpm > 250) {
-      result = `*${wpm} wpm?* Something smells fishy :fish: :face_with_monocle:`;
-    } else if (input === original) {
-      // recordkeeping
-      result = api.run("this.update_best_record", {
-        workspaceId: payload.team.id,
-        textId: metadata.textId,
-        username: payload.user.name,
-        wpm
-      })[0].message;
-    } else {
-      result = `Sorry, input did not match! *${wpm} wpm*`;
-      /* generate diff here */
-    }
-    
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  let blocks = view.blocks;	
+  // validate and determine result
+  const userInput = input.trim();
+  let result;
+
+  // advanced anti-hack detection
+  if (wpm > 250) {
+    result = `*${wpm} wpm?* Something smells fishy :fish: :face_with_monocle:`;
+  } else if (input === original) {
+    // recordkeeping
+    result = api.run("this.update_best_record", {
+      workspaceId: payload.team.id,
+      textId: metadata.textId,
+      username: payload.user.name,
+      wpm
+    })[0].message;
+  } else {
+    result = `Sorry, input did not match! *${wpm} wpm*`;
+    /* generate diff here */
+  }
+
+  let blocks = view.blocks;
   let resultBlocks = [
     blocks[0],
     blocks[1],
     {
-        "type": "section",
-        "block_id": "input",
-        "text": {
-            "type": "mrkdwn",
-            "text": `*Your Input*\n${input}`
-        }
+      type: "section",
+      block_id: "input",
+      text: {
+        type: "mrkdwn",
+        text: `*Your Input*\n${input}`
+      }
     },
     {
-        "type": "divider"
+      type: "divider"
     },
     {
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": `*Results*\n${result}`
-        }
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: `*Results*\n${result}`
+      }
     }
-  ]
-  
+  ];
+
   let resultView = {
     type: "modal",
     callback_id: "results",
     title: view.title,
-    submit:  {
+    submit: {
       type: "plain_text",
       text: "Retry"
-  	},
-    "close": {
-      "type": "plain_text",
-      "text": "Cancel"
     },
-    blocks: resultBlocks,
+    close: {
+      type: "plain_text",
+      text: "Cancel"
+    },
+    blocks: resultBlocks
   };
-  
-  return api.run("slack.views_update", {$body: {view_id: view.id, view: resultView}}, {asGroup: payload.team.id})
-}
 
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/references/js-operations
- */
+  return api.run("slack.views_update", { $body: { view_id: view.id, view: resultView } }, { asGroup: payload.team.id });
+}
