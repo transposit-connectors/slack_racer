a5b1fda2f118d2d6072746686230afbdaaca2816
Puya Moayer <puya@transposit.com>
2019-10-30 19:09:51 +0000

    Commit from Transposit IDE

---
+++ src/zzz_notes.js
@@ -0,0 +1,6 @@
+/*
+- can't include image loaded by api in loading screen because slack.views_update could finish before the dog_ceo call
+- could remove loading screen from first submit? Reponse is quick, would be smoother experience
+
+- weird block concatnation at process_test_response
+*/
