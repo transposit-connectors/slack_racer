5a5b3c6540edceb51fce31c851f86843af6ac625
Puya Moayer <puya@transposit.com>
2019-10-30 18:43:38 +0000

    Commit from Transposit IDE

--- src/process_retry_response.js
+++ src/process_retry_response.js
@@ -1,5 +1,8 @@
 (params) => {
-
+  let view = payload.view;
+  let type = view.callback_id;
+  let metadata = JSON.parse(view.private_metadata);
+  
   const viewJson = api.run("this.generate_test_view", {stringify: true, })[0]; 
   return api.run("slack.views_open", { $body: { trigger_id: payload.trigger_id, view: viewJson }}, { asGroup: body.team_id });
 }
