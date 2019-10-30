83996c3db74b5881530a321da5928ef3c84bad78
Puya Moayer <puya@transposit.com>
2019-10-30 19:44:37 +0000

    Commit from Transposit IDE

--- src/untitled_1.js
+++ src/untitled_1.js
@@ -3,7 +3,6 @@
  */
 
 ({ workspaceId, textId, username, wpm }) => {
-  
   // get workspace record for the text
   let rec = api.run("airtable.get_records", {
     baseId: env.get("baseId"),
@@ -17,9 +16,9 @@
     fields.id = workspaceId;
     fields[textId] = JSON.stringify({ username, wpm });
     api.run("airtable.create_record", { baseId: env.get("baseId"), table: "Workspaces", $body: { fields } });
-	return { updated: true, message: `Congratulations! You now hold the record for text *#${textId}* with a speed of *${wpm} wpm*! :crown: :tada:`};
+    return { updated: true, message: `Congratulations! You now hold the record for text *#${textId}* with a speed of *${wpm} wpm*! :crown: :tada:` };
   }
-  
+
   // parse json
   let blob = rec.fields[textId];
   let meta = { username: "", wpm: -1 };
@@ -41,19 +40,14 @@
       recordId: rec.id,
       $body: { fields }
     });
-    
+
     var message;
     if (oldName == "") {
       message = `Congratulations! You now hold the record for text *#${textId}* with a speed of *${wpm} wpm*! :crown: :tada:`;
     } else {
       message = `:crown: Congratulations, you beat *${oldName}* and now hold the record for text *#${textId}* with a speed of *${wpm} wpm*!`;
     }
-    return { updated: true, message};
+    return { updated: true, message };
   }
   return { updated: false, message: `Nice job! *${wpm} wpm*` };
 }
-
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/references/js-operations
- */
