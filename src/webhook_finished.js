596210fe218722fb5ac6a516881024d85ee97a0c
Puya Moayer <puya@transposit.com>
2019-10-30 19:35:02 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -24,5 +24,5 @@
   });
 
   // using response_action, must respond with everything within 3 seconds
-  return api.run("this.ack_loading_screen", {title: payload.view.title})[0];
+  return api.run("this.generate_loading_screen", {title: payload.view.title})[0];
 }
