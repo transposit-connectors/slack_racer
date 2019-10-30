a5dba078278f63adce6b8d5646949ef366a4f4af
Puya Moayer <puya@transposit.com>
2019-10-30 19:42:03 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -6,23 +6,23 @@
   let qs = require("qs.js");
   let payload = JSON.parse(http_event.parsed_body.payload);
   console.log(payload);
-  
+
   setImmediate(() => {
     switch (payload.view.callback_id) {
       case "test":
-        return api.run("this.process_test_response", {payload});
+        return api.run("this.process_test_response", { payload });
       case "results":
         let view = payload.view;
         let metadata = JSON.parse(view.private_metadata);
-        const viewJson = api.run("this.generate_test_view", {textId: metadata.textId})[0]; 
-        return api.run("slack.views_update", { $body: { view_id: view.id, view: viewJson }}, { asGroup: payload.team.id });
+        const viewJson = api.run("this.generate_test_view", { textId: metadata.textId })[0];
+        return api.run("slack.views_update", { $body: { view_id: view.id, view: viewJson } }, { asGroup: payload.team.id });
       case "loading":
-        return {status_code: 400};
+        return { status_code: 400 };
       default:
-        return {status_code: 400};
+        return { status_code: 400 };
     }
   });
 
   // using response_action, must respond with everything within 3 seconds
-  return api.run("this.generate_loading_screen", {title: payload.view.title})[0];
+  return api.run("this.generate_loading_screen", { title: payload.view.title })[0];
 }
