abe42454c743a5e73bca8e88d42be42980562122
Puya Moayer <puya@transposit.com>
2019-10-30 19:34:30 +0000

    Commit from Transposit IDE

--- src/webhook_finished.js
+++ src/webhook_finished.js
@@ -3,7 +3,6 @@
  */
 
 ({ http_event }) => {
-    // parse submission
   let qs = require("qs.js");
   let payload = JSON.parse(http_event.parsed_body.payload);
   console.log(payload);
@@ -22,7 +21,6 @@
       default:
         return {status_code: 400};
     }
-    
   });
 
   // using response_action, must respond with everything within 3 seconds
