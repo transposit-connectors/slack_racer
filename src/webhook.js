310e368784d34b94b93d6bc3a9580d97bf119a11
Puya Moayer <puya@transposit.com>
2019-10-30 19:43:48 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -6,8 +6,8 @@
   let body = http_event.parsed_body;
   setImmediate(() => {
     let textId = parseInt(body.text);
-    const viewJson = api.run("this.generate_test_view", {textId: (textId || -1)})[0]; 
-    api.run("slack.views_open", { $body: { trigger_id: body.trigger_id, view: viewJson }}, { asGroup: body.team_id });
+    const viewJson = api.run("this.generate_test_view", { textId: textId || -1 })[0];
+    api.run("slack.views_open", { $body: { trigger_id: body.trigger_id, view: viewJson } }, { asGroup: body.team_id });
   });
   return { status_code: 200 };
 }
